76bb22f93b65334682a1196d39de49efcb91f282
anhhoang236 <hoangtran2306@gmail.com>
2016-06-04 14:24:58 +0700

    hta

--- VDMVSI2/app/src/main/java/hao/bk/com/adapter/ContactItemAdapter.java
+++ VDMVSI2/app/src/main/java/hao/bk/com/adapter/ContactItemAdapter.java
@@ -1,11 +1,15 @@
 package hao.bk.com.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -18,7 +22,9 @@ import hao.bk.com.chat.ChatActivity;
 import hao.bk.com.common.ChatFilter;
 import hao.bk.com.common.ToastUtil;
 import hao.bk.com.config.Config;
+import hao.bk.com.models.CoporateNewsObj;
 import hao.bk.com.models.MemberVsiObj;
+import hao.bk.com.utils.TextUtils;
 import hao.bk.com.utils.Util;
 import hao.bk.com.vdmvsi.FragmentContactPage;
 import hao.bk.com.vdmvsi.R;
@@ -52,7 +58,7 @@ public class ContactItemAdapter extends RecyclerView.Adapter<ContactItemAdapter.
 
     public void filter(CharSequence cs) {
         listChat.clear();
-        listChat.addAll(filter.filter(cs));
+        listChat.addAll(filter.filterIncludePhone(cs));
         notifyDataSetChanged();
     }
 
@@ -64,6 +70,7 @@ public class ContactItemAdapter extends RecyclerView.Adapter<ContactItemAdapter.
     public void onBindViewHolder(ViewHolder holder, int position) {
         MemberVsiObj obj = listChat.get(position);
         holder.tvEmail.setText(obj.getUserName());
+        holder.tvPhone.setText(obj.getPhone());
         holder.index = position;
         try {
             Picasso.with(context).load(obj.getUrlThumnails())
@@ -81,6 +88,8 @@ public class ContactItemAdapter extends RecyclerView.Adapter<ContactItemAdapter.
     public class ViewHolder extends RecyclerView.ViewHolder {
         RelativeLayout lnlRoot;
         TextView tvEmail;
+        TextView tvPhone;
+        ImageButton btnCall;
         int index;
         CircleImageView avatarUser;
 
@@ -104,7 +113,44 @@ public class ContactItemAdapter extends RecyclerView.Adapter<ContactItemAdapter.
             });
             avatarUser = (CircleImageView) itemView.findViewById(R.id.imv_profile);
             tvEmail = (TextView) itemView.findViewById(R.id.tv_name_user);
+            tvPhone = (TextView) itemView.findViewById(R.id.tv_phone);
+            btnCall = (ImageButton) itemView.findViewById(R.id.btn_call);
+            btnCall.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    callUser(index);
+                }
+            });
         }
 
+    }
+
+    public void callUser(int index) {
+        // Xử lý goi ng dang tin
+        MemberVsiObj obj =  listChat.get(index);
+        if (TextUtils.isEmpty(obj.getPhone())) {
+            toastUtil.showToast(context.getString(R.string.txt_not_phone));
+        } else {
+            final String x = obj.getPhone();
+            AlertDialog.Builder alBuilder = new AlertDialog.Builder(
+                    context);
+            alBuilder.setMessage(context.getString(R.string.txt_message) + " " + obj.getPhone() + " " + context.getString(R.string.txt_no) + " ?");
+            final AlertDialog.Builder builder = alBuilder.setPositiveButton(R.string.txt_yes, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                    callIntent.setData(Uri.parse("tel:" + x));
+                    context.startActivity(callIntent);
+                }
+            });
+            alBuilder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            AlertDialog alertDialog = alBuilder.create();
+            alertDialog.show();
+        }
     }
 }
--- VDMVSI2/app/src/main/java/hao/bk/com/common/ChatFilter.java
+++ VDMVSI2/app/src/main/java/hao/bk/com/common/ChatFilter.java
@@ -6,26 +6,41 @@ import hao.bk.com.models.MemberVsiObj;
 
 public class ChatFilter {
 
-        private ArrayList<MemberVsiObj> originalList;
+    private ArrayList<MemberVsiObj> originalList;
 
-        public ChatFilter(ArrayList<MemberVsiObj> originalList) {
-            this.originalList = new ArrayList<>();
-            this.originalList.addAll(originalList);
-        }
+    public ChatFilter(ArrayList<MemberVsiObj> originalList) {
+        this.originalList = new ArrayList<>();
+        this.originalList.addAll(originalList);
+    }
 
-        public ArrayList<MemberVsiObj> filter(CharSequence constraint) {
-            ArrayList<MemberVsiObj> filteredList = new ArrayList<>();
-            if (constraint.length() == 0) {
-                filteredList.addAll(originalList);
-            } else {
-                String filterPattern = constraint.toString().toLowerCase().trim();
-                for (MemberVsiObj obj : originalList) {
-                    if (obj.getUserName().contains(filterPattern)) {
-                        filteredList.add(obj);
-                    }
+    public ArrayList<MemberVsiObj> filter(CharSequence constraint) {
+        ArrayList<MemberVsiObj> filteredList = new ArrayList<>();
+        if (constraint.length() == 0) {
+            filteredList.addAll(originalList);
+        } else {
+            String filterPattern = constraint.toString().toLowerCase().trim();
+            for (MemberVsiObj obj : originalList) {
+                if (obj.getUserName().contains(filterPattern)) {
+                    filteredList.add(obj);
                 }
             }
-            return filteredList;
         }
-
+        return filteredList;
     }
+
+    public ArrayList<MemberVsiObj> filterIncludePhone(CharSequence constraint) {
+        ArrayList<MemberVsiObj> filteredList = new ArrayList<>();
+        if (constraint.length() == 0) {
+            filteredList.addAll(originalList);
+        } else {
+            String filterPattern = constraint.toString().toLowerCase().trim();
+            for (MemberVsiObj obj : originalList) {
+                if (obj.getUserName().contains(filterPattern)||obj.getPhone().contains(filterPattern)) {
+                    filteredList.add(obj);
+                }
+            }
+        }
+        return filteredList;
+    }
+
+}
